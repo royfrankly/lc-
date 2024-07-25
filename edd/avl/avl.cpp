58b520d1ee27cf31f948ee4e1ddae5043d87a343
WninaE <wninae@unjbg.edu.pe>
2024-07-25 10:50:52 -0500

    m avl no corre

--- edd/avl/avl.cpp
+++ edd/avl/avl.cpp
@@ -4,13 +4,12 @@ struct nodo{
     int inf,fe;
     nodo *izq,*der;
 };
-void inserta_AVL(nodo *&raiz,int dato,int &cen);
+void inserta_AVL(nodo *&raiz,int dato,int cen);
 void buscar_AVL(nodo *raiz, int dato);
 void elimina_AVL(nodo *&raiz,int &cen, int dato);
 void preorden(nodo *raiz);
 void inorden(nodo *raiz);
 void postorden(nodo *raiz);
-
 void borra(nodo *&aux,nodo *&q,int &cen);
 void reestructura1(nodo *&raiz, int &cen);
 void reestructura2(nodo *&raiz, int &cen);
@@ -22,17 +21,18 @@ int main(void){
     system("cls");
     do{
         system("cls");
-        std::cout<<"\t\t\tMENU - ARBOLES BINARIOS BALANCEADOS - AVL\n\n";
-        std::cout<<"\t\t\t******************************************\n\n";
-        std::cout<<"\t\t1. INSERCION\n\n";
-        std::cout<<"\t\t2. ELIMINACION\n\n";
-        std::cout<<"\t\t3. BUSQUEDA\n\n";
-        std::cout<<"\t\t4. RECORRIDO PREORDEN\n\n";
-        std::cout<<"\t\t5. RECORRIDO INORDEN\n\n";
-        std::cout<<"\t\t6. RECORRIDO POSTORDEN\n";
-        std::cout<<"\t\t\t\tS A L I R\n\n";
-        std::cout<<"\t\t\t\tOPCION  ";
-        std::cin>>op;
+        cout<<"\t\t\tMENU - ARBOLES BINARIOS BALANCEADOS - AVL\n\n";
+        cout<<"\t\t\t******************************************\n\n";
+        cout<<"\t\t1. INSERCION\n\n";
+        cout<<"\t\t2. ELIMINACION\n\n";
+        cout<<"\t\t3. BUSQUEDA\n\n";
+        cout<<"\t\t4. RECORRIDO PREORDEN\n\n";
+        cout<<"\t\t5. RECORRIDO INORDEN\n\n";
+        cout<<"\t\t6. RECORRIDO POSTORDEN\n\n";
+        cout<<"\t\t7. TODOS LOS RECORRIDOS\n\n";
+        cout<<"\t\t8. S A L I R\n\n";
+        cout<<"\t\t\t\tOPCION  ";
+        cin>>op;
         system("cls");
         switch(op)
         {
@@ -40,59 +40,70 @@ int main(void){
             do
             {
                 system("cls");
-                std::cout << "\n\n INGRESE EL DATO A INSERTAR: ";
-                std::cin >> dato;
-                int cen1 = 0;
-                inserta_AVL(raiz, dato, cen1);
-                std::cout << "\n\t\t¿MAS DATOS? (S/N): ";
-                std::cin >> opc;
+                cout << "\n\n INGRESE EL DATO A INSERTAR: ";
+                cin >> dato;
+                
+                inserta_AVL(raiz, dato, 1);
+                cout << "\n\t\t¿MAS DATOS? (S/N): ";
+                cin >> opc;
             } while(opc == 's' || opc == 'S');
             system("pause");
             break;
         case 2:
             system("cls");
             cen2=1;
-            std::cout << "\n\nINGRESE EL DATO A ELIMINAR: ";
-            std::cin >> dato;
+            cout << "\n\nINGRESE EL DATO A ELIMINAR: ";
+            cin >> dato;
             
             elimina_AVL(raiz,cen2, dato);
             system("pause");
             break;
         case 3:
             system("cls");
-            std::cout << "\n\nINGRESE EL DATO A BUSCAR: ";
-            std::cin >> dato;
+            cout << "\n\nINGRESE EL DATO A BUSCAR: ";
+            cin >> dato;
             buscar_AVL(raiz, dato);
             system("pause");
             break;
         case 4:
             system("cls");
-            std::cout << "\n\tRECORRIDO PREORDEN";
-            std::cout << "\n\t******************\n\n";
+            cout << "\n\tRECORRIDO PREORDEN";
+            cout << "\n\t******************\n\n";
             preorden(raiz);
             system("pause");
             break;
         case 5:
             system("cls");
-            std::cout << "\n\tRECORRIDO INORDEN";
-            std::cout << "\n\t*******************\n\n";
+            cout << "\n\tRECORRIDO INORDEN";
+            cout << "\n\t*******************\n\n";
             inorden(raiz);
             system("pause");
             break;
         case 6:
             system("cls");
-            std::cout << "\n\tRECORRIDO INORDEN";
-            std::cout << "\n\t*******************\n\n";
+            cout << "\n\tRECORRIDO POSTORDEN";
+            cout << "\n\t*******************\n\n";
             postorden(raiz);
             system("pause");
             break;
+        case 7:
+            cout << "\n\tRECORRIDO PREORDEN";
+            cout << "\n\t******************\n\n";
+            preorden(raiz);
+            cout << "\n\tRECORRIDO INORDEN";
+            cout << "\n\t*******************\n\n";
+            inorden(raiz);
+            cout << "\n\tRECORRIDO POSTORDEN";
+            cout << "\n\t*******************\n\n";
+            postorden(raiz);
+            system("pause");
         }
-    }while(op<7);
+    }while(op<8);
     return 0;
 }
 
 
-void inserta_AVL(nodo *&raiz, int dato, int &cen){
+void inserta_AVL(nodo *&raiz, int dato, int cen){
     nodo *raiz1, *raiz2,*x;
     if(raiz!=NULL){
         if(dato<raiz->inf){
@@ -165,7 +176,7 @@ void inserta_AVL(nodo *&raiz, int dato, int &cen){
             }
         }
         else
-            std::cout<<"\nEL DATO YA EXISTE ... \n\n";
+            cout<<"\nEL DATO YA EXISTE ... \n\n";
     }else{
         x=new nodo;
         x->inf=dato;
@@ -179,17 +190,17 @@ void inserta_AVL(nodo *&raiz, int dato, int &cen){
 void buscar_AVL(nodo *raiz, int dato){
     if(dato<raiz->inf)
         if(raiz->izq==NULL)
-            std::cout<<"\n\tEL DATO BUSCADO NO EXISTE.... \n\n";
+            cout<<"\n\tEL DATO BUSCADO NO EXISTE.... \n\n";
         else 
             buscar_AVL(raiz->izq, dato);
     else
         if(dato>raiz->inf)
             if(raiz->der==NULL)
-                std::cout<<"\n\tEL DATO BUSCADO NO EXISTE....\n\n";
+                cout<<"\n\tEL DATO BUSCADO NO EXISTE....\n\n";
             else
                 buscar_AVL(raiz->der,dato);
         else
-            std::cout<<"\n\tEL DATO BUSCADO SI EXISTE....\n\n";
+            cout<<"\n\tEL DATO BUSCADO SI EXISTE....\n\n";
 }
 
 void elimina_AVL(nodo *&raiz,int dato){
@@ -197,7 +208,7 @@ void elimina_AVL(nodo *&raiz,int dato){
 }
 void preorden(nodo *raiz){
     if(raiz!=NULL){
-        std::cout<<raiz->inf<<"\n\n";
+        cout<<raiz->inf<<"\n\n";
         preorden(raiz->izq);
         preorden(raiz->der);
     }
@@ -206,7 +217,7 @@ void preorden(nodo *raiz){
 void inorden(nodo *raiz){
     if(raiz!=NULL){
         inorden(raiz->izq);
-        std::cout<<raiz->inf<<"\n\n";
+        cout<<raiz->inf<<"\n\n";
         inorden(raiz->der);
     }
 }
@@ -215,7 +226,7 @@ void postorden(nodo *raiz){
     if(raiz!=NULL){
         postorden(raiz->izq);
         postorden(raiz->der);
-        std::cout<<raiz->inf<<"\n\n";
+        cout<<raiz->inf<<"\n\n";
     }
 }
 
@@ -247,7 +258,7 @@ void elimina_AVL(nodo *&raiz,int &cen, int dato){
             }
         }
     }else{
-        std::cout<<"el dato no existe"<<endl;
+        cout<<"el dato no existe"<<endl;
     }
 }
 
